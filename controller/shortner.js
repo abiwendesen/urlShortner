29e0b5f336d062cc7336a9345959615d9907bdbb
abiwendesen21 <abiwendesen21@gmail.com>
2026-02-05 23:09:43 +0300

    corrected the caching logic

--- controller/shortner.js
+++ controller/shortner.js
@@ -2,6 +2,7 @@ import { db } from "../db/database.js"
 import { encode ,decode} from "../utills/base62.js";
 import client from "../db/cache.js";
 
+
 const CACHE_TLL = 3600
 export const urlshort = async(req,res)=>{
     const longUrl = req.body.url;
@@ -12,6 +13,7 @@ export const urlshort = async(req,res)=>{
      const insertedId = result.insertId;
      const base = encode(insertedId);
      const shorturl = process.env.BASE_URL + base;
+    
      try{
         await client.set(`short:${shorturl}`,longUrl,{
             EX:CACHE_TLL
@@ -23,29 +25,36 @@ export const urlshort = async(req,res)=>{
 
     const[insert] = await db.query("UPDATE shorturl SET short_code=? WHERE id=?", [shorturl,insertedId])
       
-
+   if(insert){
     return res.status(200).json({message: shorturl})
-    
+   }
 }
 
 
 export const getUrl = async(req,res)=> {
     const url = req.originalUrl;
+
     const idLookUp = url.split('/');
-    const decodedId  = decode(idLookUp);
-    console.log(decodedId)
-    const cachedUrl = await client.get(`short:${process.env.BASE_URL +url}`);
-
-    if(!cachedUrl){
-    const [result] = await db.query("SELECT long_url from shorturl where id = ?", [decodedId]);
-     const longUrl = result[0].long_url;
-        await client.set(`short:${process.env.BASE_URL +url}`,longUrl);
-         res.status(302).json({message: result[0]})
-        console.log('MISS Wrote on redis')
+    console.log("h"+url)
+  
+    const decodedId  = decode(idLookUp[1]);
+  
+   try{
+    const cachedUrl = await client.get(`short:${process.env.BASE_URL +idLookUp[1]}`);
+   // console.log(cachedUrl)
+    if(cachedUrl){
+        res.status(302).json({message: cachedUrl})
     }
-   
-
-    // console.log(result[0])
+    const [result] = await db.query("SELECT long_url from shorturl where id=?",[decodedId]);
+    
+    await client.set(`short:${process.env.BASE_URL +idLookUp[1]}`,result[0].long_url,{
+        EX: CACHE_TLL
+    })
+   // console.log(result[0])
+   }
+   catch(err){
+    console.log("Warning? " + err)
+   }
 
     
 }
